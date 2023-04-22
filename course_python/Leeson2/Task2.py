ca9912f58fcc75f548048f84b1786be54418683d
mayakoff96 <108720732+mayakoff96@users.noreply.github.com>
2023-04-22 17:06:51 +0300

    Delete course_python/Leeson2 directory

--- course_python/Leeson2/Task2.py
+++
@@ -1,9 +0,0 @@
-print('Hello World')
-
-print(1)
-print(2)
-print(3)
-
-print('Вода', 'Компот', 'Кола', 'Фанта',sep=',')
-print('Вода', 'Компот', 'Кола', 'Фанта',sep='-', end='.\n')
-print(4, 5, 6, sep=' ', end='.\n')
