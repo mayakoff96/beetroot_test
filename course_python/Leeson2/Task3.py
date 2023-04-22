10ffd210128bba247ca05b192d74e3d6eda28519
dmytro <mayakoff96@gmail.com>
2023-04-22 14:01:09 +0300

    Lesson_2

---
+++ course_python/Leeson2/Task3.py
@@ -0,0 +1,17 @@
+a = '#'
+
+# Print shape 0
+
+print(a * 9 + '\n' +
+      a + '\t\t' + a + '\n' +
+      a + '\t\t' + a + '\n' +
+      a + '\t\t' + a + '\n' +
+      a * 9)
+print('')
+
+#Print shape H
+print(a + '\t\t' + a + '\n' +
+      a + '\t\t' + a + '\n'+
+      a * 9 + '\n' +
+      a + '\t\t' + a + '\n' +
+      a + '\t\t' + a)
