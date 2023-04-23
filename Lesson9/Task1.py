1a7f68d7ef7b48cd51aa217db8afce03acf2e5a9
dmytro <mayakoff96@gmail.com>
2023-04-23 14:12:17 +0300

    Homework,lesson9

---
+++ Lesson9/Task1.py
@@ -0,0 +1,7 @@
+from modules.module_1 import greeting
+from modules.module_2 import day_today
+
+
+name = input('What is your name? ')
+greeting(name)
+day_today()
---
+++ Lesson9/Task2.py
@@ -0,0 +1,7 @@
+import sys
+
+print(sys.path)
+
+sys.path.append('/home/beetroot_test/modules')
+
+print(sys.path)
