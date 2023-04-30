ed1ec356a9385ea056b14d3e8ab719344e65ed5f
dmytro <mayakoff96@gmail.com>
2023-04-30 11:48:50 +0300

    Fix for homework,lesson4,task4

--- Lesson4/Task4.py
+++ Lesson4/Task4.py
@@ -1,9 +1,9 @@
 name = 'dmytro'
 inp_name = input('Enter your name: ')
 
-if inp_name.lower == name \
-        or inp_name.upper() \
-        or inp_name.capitalize():
+if inp_name.lower() == name \
+        or inp_name.upper() == name\
+        or inp_name.capitalize() == name:
     print(True)
 else:
     print(False)
